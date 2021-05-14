f302da407e798db1bc909a9bf90bbce9d38315a4
sunn-e <sunnydhoke22@gmail.com>
2021-05-14 23:39:13 +0530

    Tried electron build, it sucks. Works during development but executables fail.

---
+++ yoapp-win32-x64/resources/app/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Go away, nothing to see here. :p </Text>
+      <Text>Well, now I'm just trying to trigger GitHub Action </Text>
+      <Text> Lmao,electron is up with "yarn expo-electron start"</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
